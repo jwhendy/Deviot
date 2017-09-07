cc3038f1a5179e542af5fde01258fe6cb631a1ff
John Henderson <jw.hendy@gmail.com>
2017-09-07 15:35:26 -0500

    changed version string for develop branch

--- libraries/__init__.py
+++ libraries/__init__.py
@@ -1,6 +1,6 @@
 VERSION = (2, 1, 1)
 __version__ = ".".join([str(s) for s in VERSION])
-
+# __version__ = "develop-branch"
 __title__ = "Deviot"
 __description__ = (
     "Plugin for IoT development based in the platformIO ecosystem."
